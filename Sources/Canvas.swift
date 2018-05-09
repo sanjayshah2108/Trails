89cdab0a6230429f9f406e6916fd6f5bb46da25f
Sanjay Shah <Sanjay Shah>
2018-05-09 10:24:25 -0700

    Added collision logic

--- Sources/Canvas.swift
+++ Sources/Canvas.swift
@@ -3,13 +3,26 @@
 import Cocoa
 import QuartzCore
 
+protocol CanvasDelegate {
+    func presentCollisionAlert()
+}
+
 /// A view that draws a projectile as a circle.
 class Canvas: NSView {
     /// The projectile color.
-    let projectileColor = NSColor.blue
+    let blueProjectileColor = NSColor.blue
+    let redProjectileColor = NSColor.red
+    
+    var canvasDelegate: CanvasDelegate!
 
     /// The projectile to draw.
-    var projectile = Projectile() {
+    var blueProjectile = Projectile() {
+        didSet {
+            setNeedsDisplay(bounds)
+        }
+    }
+    
+    var redProjectile = Projectile() {
         didSet {
             setNeedsDisplay(bounds)
         }
@@ -18,11 +31,33 @@ class Canvas: NSView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
-        let rect = CGRect(x: projectile.position.x - projectile.radius, y: projectile.position.y - projectile.radius, width: 2*projectile.radius, height: 2*projectile.radius)
-        let path = NSBezierPath(ovalIn: rect)
+        let blueRect = CGRect(x: blueProjectile.position.x - blueProjectile.radius, y: blueProjectile.position.y - blueProjectile.radius, width: 2*blueProjectile.radius, height: 2*blueProjectile.radius)
+        let bluePath = NSBezierPath(ovalIn: blueRect)
 
-        projectileColor.setFill()
-        path.fill()
+        blueProjectileColor.setFill()
+        bluePath.fill()
+        
+        
+        let redRect = CGRect(x: redProjectile.position.x - redProjectile.radius, y: redProjectile.position.y - redProjectile.radius, width: 2*redProjectile.radius, height: 2*redProjectile.radius)
+        let redPath = NSBezierPath(ovalIn: redRect)
+        
+        redProjectileColor.setFill()
+        redPath.fill()
+        
+        
+        if let blueWasShot = blueHasBeenShot, blueWasShot == true {
+            
+            if let redWasShot = redHasBeenShot, redWasShot == true {
+                
+                blueHasBeenShot = false
+                redHasBeenShot = false
+        
+            //collision detection
+            if (redRect.intersects(blueRect)){
+                canvasDelegate.presentCollisionAlert()
+            }
+            }
+        }
     }
     
 }
--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -2,19 +2,31 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-    //
+public var blueHasBeenShot: Bool?
+public var redHasBeenShot: Bool?
+
+class ViewController: NSViewController, CanvasDelegate {
+    
     /// How often to execute an update.
     let updateInterval = 1 / 60 as TimeInterval
 
-    @IBOutlet weak var vxTextField: NSTextField!
-    @IBOutlet weak var vyTextField: NSTextField!
-    @IBOutlet weak var axTextField: NSTextField!
-    @IBOutlet weak var ayTextField: NSTextField!
+    @IBOutlet weak var vxBlueTextField: NSTextField!
+    @IBOutlet weak var vyBlueTextField: NSTextField!
+    @IBOutlet weak var axBlueTextField: NSTextField!
+    @IBOutlet weak var ayBlueTextField: NSTextField!
+    
+    @IBOutlet weak var vxRedTextField: NSTextField!
+    @IBOutlet weak var vyRedTextField: NSTextField!
+    @IBOutlet weak var axRedTextField: NSTextField!
+    @IBOutlet weak var ayRedTextField: NSTextField!
+    
+    
+    
     @IBOutlet weak var canvas: Canvas!
 
-    /// The model object representing the projectile.
-    var projectile = Projectile()
+    /// The models object representing the projectiles.
+    var blueProjectile = Projectile()
+    var redProjectile = Projectile()
 
     /// Last time we did an update.
     var lastUpdate = Date.distantPast
@@ -23,15 +35,31 @@ class ViewController: NSViewController {
     weak var timer: Timer?
 
     @IBAction func shoot(_ sender: NSButton) {
-        projectile.position = .zero
-        projectile.velocity = CGVector(dx: vxTextField.doubleValue, dy: vyTextField.doubleValue)
-        projectile.acceleration = CGVector(dx: axTextField.doubleValue, dy: ayTextField.doubleValue)
+        blueProjectile.position = .zero
+        blueProjectile.velocity = CGVector(dx: vxBlueTextField.doubleValue, dy: vyBlueTextField.doubleValue)
+        blueProjectile.acceleration = CGVector(dx: axBlueTextField.doubleValue, dy: ayBlueTextField.doubleValue)
         
-        timer = Timer.scheduledTimer(timeInterval: updateInterval, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: updateInterval, target: self, selector: #selector(updateBlue), userInfo: nil, repeats: true)
+        
+        blueHasBeenShot = true
     }
-
+    
+    
+    @IBAction func shootRed(_ sender: NSButton) {
+        redProjectile.position = .zero
+        redProjectile.velocity = CGVector(dx: vxBlueTextField.doubleValue, dy: vyBlueTextField.doubleValue)
+        redProjectile.acceleration = CGVector(dx: axBlueTextField.doubleValue, dy: ayBlueTextField.doubleValue)
+        
+        timer = Timer.scheduledTimer(timeInterval: updateInterval, target: self, selector: #selector(updateBlue), userInfo: nil, repeats: true)
+        
+        redHasBeenShot = true
+    }
+    
     /// Perform an update by stepping the physics forward.
-    @objc func update() {
+    @objc func updateBlue() {
+        
+        canvas.canvasDelegate = self
+        
         let date = Date()
         let Δt = date.timeIntervalSince(lastUpdate)
         lastUpdate = date
@@ -40,11 +68,56 @@ class ViewController: NSViewController {
             // Avoid big time gaps
             return
         }
-        projectile.step(Δt: CGFloat(Δt))
-        canvas.projectile = projectile
+        
+        blueProjectile.step(Δt: CGFloat(Δt))
+        canvas.blueProjectile = blueProjectile
 
-        if !canvas.bounds.contains(projectile.position) {
+        if !canvas.bounds.contains(blueProjectile.position) {
             timer?.invalidate()
         }
+        
+        redProjectile.step(Δt: CGFloat(Δt))
+        canvas.redProjectile = redProjectile
+        
+        if !canvas.bounds.contains(redProjectile.position) {
+            timer?.invalidate()
+        }
+        
+        
+    }
+    
+    @objc func updateRed() {
+        let date = Date()
+        let Δt = date.timeIntervalSince(lastUpdate)
+        lastUpdate = date
+        
+        if Δt > 0.5 {
+            // Avoid big time gaps
+            return
+        }
+        
+        
+        redProjectile.step(Δt: CGFloat(Δt))
+        canvas.redProjectile = redProjectile
+        
+        if !canvas.bounds.contains(redProjectile.position) {
+            timer?.invalidate()
+        }
+    }
+    
+    func presentCollisionAlert(){
+        let alert = NSAlert()
+        alert.messageText = "BANG!"
+        alert.informativeText = "The projectiles collided"
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "OK, Restart")
+   
+        alert.beginSheetModal(for: self.view.window!, completionHandler: {(response) in
+            
+            //restart 
+            self.blueProjectile.position = CGPoint.zero
+            self.redProjectile.position = CGPoint.zero
+  
+        })
     }
 }
